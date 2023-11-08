d72cb9cb54b57c0329509d0f066b13c5e5c4ba86
LucasVergaraV <luca.vergara@duocuc.cl>
2023-11-08 01:26:37 -0300

    se agrega la funcionalidad de registrarse

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,13 +1,16 @@
 import { Injectable, inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword ,createUserWithEmailAndPassword ,updateProfile  } from 'firebase/auth';
 import { User } from '../models/user.model';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { getFirestore, setDoc, doc } from '@angular/fire/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
   auth = inject(AngularFireAuth);
+  firestore = inject(AngularFirestore);
 
 //-----Autentificacion---------
 
@@ -19,4 +22,21 @@ singIn(user: User){
   return signInWithEmailAndPassword(getAuth(),user.email,user.password);
 }
 
+// ----Registrarse
+singUp(user: User){
+  return createUserWithEmailAndPassword(getAuth(),user.email,user.password);
+}
+
+// ----Editar usuario------
+updateUser(displayName: string){
+  return updateProfile(getAuth().currentUser,{displayName});
+}
+// ----------------------------------------------------------
+// ----------------------------------------------------------
+// ----------------------------------------------------------
+// ----------------------------------------------------------
+// ------BASE DE DATOS--------
+setDocument(path: string, data: any){
+  return setDoc(doc(getFirestore(), path), data);
+}
 }
--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { LoadingController, ToastController, ToastOptions } from '@ionic/angular';
 
 @Injectable({
@@ -8,6 +9,7 @@ export class UtilsService {
 
   loadingCtrl = inject(LoadingController)
   toastCtrl = inject(ToastController)
+  router = inject(Router);
 
 // -----LOADING-------
   loading(){
@@ -18,5 +20,21 @@ export class UtilsService {
  async presentToast(opts?: ToastOptions) {
   const toast = await this.toastCtrl.create(opts);
   toast.present();
+ }
+
+//----ROUTERLINK--------
+//Enrutamiento de cualquier pagina disponible
+ routerLink(url: string){
+  return this.router.navigateByUrl(url);
+ }
+
+//-----LOCALSTORAGE------
+//Permite guardar cualquier dato o arreglo del sistema
+ saveInLocalStorage(key: string, value: any){
+  return localStorage.setItem(key, JSON.stringify(value));
+ }
+//Permite recuperar informacion desde el localstorage
+ getFromLocalStorage(key: string){
+  return JSON.parse(localStorage.getItem(key)); 
  }
 }
